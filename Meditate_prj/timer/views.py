ff7ee900b1042980c4f3c0b53eb7c45f809d38eb
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-07 12:02:01 +0100

    setup initial view

--- Meditate_prj/timer/views.py
+++ Meditate_prj/timer/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def get_timer(request):
+    return HttpResponse('Timer goes here')
