6d074f3433fb4fb5738f160904e054cf081369fc
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-07 14:25:55 +0100

    set up bells for the timer

--- Meditate_prj/static/js/timer.js
+++ Meditate_prj/static/js/timer.js
@@ -2,7 +2,7 @@ var counter =  moment.utc(180*1000).format('mm:ss');
 var t;
 var isTimerOn = false;
 
-var tone = new Audio("http://kencodes.com/bong.mp3");
+var tone = document.getElementById("bell-one");;
 
 
 function countdown() {
@@ -10,7 +10,7 @@ function countdown() {
     console.log(counter);
     counter--;
     t = setTimeout("countdown();", 1000);
-    
+
     if (counter < 0) {
       stopMe();
       tone.play();
@@ -18,6 +18,8 @@ function countdown() {
 }
 
 function startMe() {
+    tone.pause();
+    tone.currentTime = 0;
     if (!isTimerOn) {
       isTimerOn = true;
       tone.play();
@@ -85,3 +87,34 @@ function setFifteenMin() {
     return counter;
 }
 
+function setBellOne() {
+    tone.pause();
+    tone.currentTime = 0;
+    tone = document.getElementById("bell-one");
+    tone.play();
+    return tone
+}
+
+function setBellTwo() {
+    tone.pause();
+    tone.currentTime = 0;
+    tone = document.getElementById("bell-two");
+    tone.play();
+    return tone
+}
+
+function setBellThree() {
+    tone.pause();
+    tone.currentTime = 0;
+    tone = document.getElementById("bell-three");
+    tone.play();
+    return tone
+}
+
+function setBellFour() {
+    tone.pause();
+    tone.currentTime = 0;
+    tone = document.getElementById("bell-four");
+    tone.play();
+    return tone
+}
