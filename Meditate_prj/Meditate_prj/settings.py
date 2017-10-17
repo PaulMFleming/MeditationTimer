4a58c1179e7c8d055457e0aed374d68fe3a0cfd9
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-17 17:19:29 +0100

    updated media and static files to AWS S3

--- Meditate_prj/Meditate_prj/settings.py
+++ Meditate_prj/Meditate_prj/settings.py
@@ -153,7 +153,11 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
